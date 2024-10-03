edf6a4d56afc698cadd1b819c32a13cfd704e0cf
Henrik Soerensen <henrik@voi.network>
2024-10-03 12:03:00 -0400

    - Add copy to shell command - Minor typographical improvement in staking forecast page

--- apps/web/src/Components/ShellCommand/ShellCommand.tsx
+++ apps/web/src/Components/ShellCommand/ShellCommand.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, Button } from '@mui/material';
+import { useState } from 'react';
+import { CopyAll } from '@mui/icons-material';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { solarizedlight } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
@@ -10,7 +12,20 @@ interface ShellCommandProps {
 }
 
 const ShellCommand: React.FC<ShellCommandProps> = ({ command, description }) => {
-  return (
+    const [copied, setCopied] = useState(false);
+
+    const handleCopy = () => {
+        navigator.clipboard.writeText(command)
+            .then(() => {
+                setCopied(true);
+                setTimeout(() => setCopied(false), 3000);
+            })
+            .catch(err => {
+                console.error('Failed to copy text: ', err);
+            });
+    };
+
+    return (
     <Box sx={{ margin: '20px 0' }}>
       <Typography variant="h6" gutterBottom>
         {description}
@@ -18,6 +33,15 @@ const ShellCommand: React.FC<ShellCommandProps> = ({ command, description }) => 
       <SyntaxHighlighter language="bash" style={solarizedlight}>
         {command}
       </SyntaxHighlighter>
+      <Button
+        variant="contained"
+        color="primary"
+        startIcon={<CopyAll />}
+        onClick={handleCopy}
+        sx={{ marginTop: '10px' }}
+        >
+        {copied ? 'Copied!' : 'Copy'}
+      </Button>
     </Box>
   );
 };
